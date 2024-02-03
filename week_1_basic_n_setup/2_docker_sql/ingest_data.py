17d3df6a55b4d451b2444a1817f087dc0ae7c12d
Wahyu Tri Utomo <wahyutri66773@gmail.com>
2024-02-03 16:32:24 +0700

    create docker-compose for postgres & pgadmin

--- week_1_basic_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basic_n_setup/2_docker_sql/ingest_data.py
@@ -34,17 +34,20 @@ def main(params):
 
     df.to_sql(con=engine, name=table_name, if_exists='append')
 
-    while True:
-        t_start = time()
-        df = next(df_iter)
-        
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        
-        df.to_sql(con=engine, name=table_name, if_exists='append')
-        t_end = time()
-        
-        print('inserted another chunk..., took %.3f seconds' %(t_end - t_start))
+    try:
+        while True:
+            t_start = time()
+            df = next(df_iter)
+            
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            
+            df.to_sql(con=engine, name=table_name, if_exists='append')
+            t_end = time()
+            
+            print('inserted another chunk..., took %.3f seconds' %(t_end - t_start))
+    except:
+        print('there is no data left, ingesting done!')
 
 
 if __name__ == '__main__':
